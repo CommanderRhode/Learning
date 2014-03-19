d9c1dc7d61fa564fc8f8130e8e4b336fde89a4ff
CommanderRhode <forted03@mc-s056904.local>
2014-03-19 19:04:14 +0000

    3rd test passes

--- Katas/Bowling/javaScript/Bowling/src/BowlingGame.js
+++ Katas/Bowling/javaScript/Bowling/src/BowlingGame.js
@@ -10,17 +10,26 @@
 function bowlingGame() {
 
 	this.score = 0;
+	this.rolls = [];
 
 };
 
 bowlingGame.prototype = {
 
 	scoreBowling: function() {
+		for (var roll = 0; roll < this.rolls.length; roll++)
+		{
+			this.score += this.rolls[roll];
+			if (this.rolls[roll -1] + this.rolls[roll - 2] == 10)
+			{
+				this.score += this.rolls[roll];
+			}
+		}
 		return this.score;
 	},
 
 	roll: function(pins) {
-		this.score += pins;
+		this.rolls.push(pins);
 	}
 };
 
