5d751901ca129cfe9d686c31e648f8ed122c713c
CommanderRhode <forted03@mc-s056904.local>
2014-03-19 18:23:46 +0000

    2nd failing test

--- Katas/Bowling/javaScript/Bowling/src-test/BowlingGameTest.js
+++ Katas/Bowling/javaScript/Bowling/src-test/BowlingGameTest.js
@@ -7,3 +7,8 @@ BowlingGameTest.prototype.testGutterGame = function() {
 	var game = new bowlingGame("GutterGame");
 	assertEquals(0, game.scoreBowling());
 };
+
+BowlingGameTest.prototype.testOnePerThrowGame = function() {
+	var game = new bowlingGame("OnePerThrowGame");
+	assertEquals(20, game.scoreBowling());
+}
