55b4f162d1e445cfb2896f3420448d1338f5e9e0
CommanderRhode <forted03@mc-s056904.local>
2014-03-19 18:08:37 +0000

    adding base files and jstestdriver

---
+++ Katas/Bowling/javaScript/Bowling/src-test/BowlingGameTest.js
@@ -0,0 +1,9 @@
+/**
+ * JS Test Driver unit tests for Bowling Game.
+ */
+BowlingGameTest = TestCase("BowlingGame");
+
+BowlingGameTest.prototype.testStub = function() {
+	// TODO: Implement some tests
+	// expectEquals(x, y);
+};
---
+++ Katas/Bowling/javaScript/Bowling/src/BowlingGame.js
@@ -0,0 +1,21 @@
+/**
+ * Bowling Game
+ */
+
+
+/**
+ * Constructor
+ * @param {String}
+ */
+function bowlingGame() {
+
+};
+
+bowlingGame.prototype = {
+
+	scoreBowling: function() {
+		// TODO: Calculate score for bowling game
+	}
+};
+
+
