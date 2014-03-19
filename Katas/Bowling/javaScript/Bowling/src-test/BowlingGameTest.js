970fab6807f1711b08d01a8ad7dbaf7832538043
CommanderRhode <forted03@mc-s056904.local>
2014-03-19 18:21:49 +0000

    guttergame failing test

--- Katas/Bowling/javaScript/Bowling/src-test/BowlingGameTest.js
+++ Katas/Bowling/javaScript/Bowling/src-test/BowlingGameTest.js
@@ -3,8 +3,7 @@
  */
 BowlingGameTest = TestCase("BowlingGame");
 
-BowlingGameTest.prototype.testStub = function() {
-	// TODO: Implement some tests
-	// expectEquals(x, y);
-	assertTrue(false);
+BowlingGameTest.prototype.testGutterGame = function() {
+	var game = new bowlingGame("GutterGame");
+	assertEquals(0, game.scoreBowling());
 };
